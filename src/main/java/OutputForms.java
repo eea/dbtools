9046706aeba8fbaf2e2b5b15904cd055ab956e18
Søren Roug <soren.roug@eea.europa.eu>
2015-07-22 21:38:12 +0200

    Added types to Excel cells.

--- src/main/java/OutputForms.java
+++ src/main/java/OutputForms.java
@@ -1,12 +1,16 @@
 import java.io.PrintStream;
+import java.sql.Clob;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.Date;
 
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVFormat;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -60,23 +64,57 @@ enum OutputForms {
     },
     EXCEL {
         /**
-         * Output the result set in TSV format with \N for nulls.
+         * Output the result set in Excel Open XML format.
          */
         void output(ResultSet rs, PrintStream console) throws Exception {
             ResultSetMetaData rsMd = rs.getMetaData();
             int columnCount = rsMd.getColumnCount();
             Workbook wb = new XSSFWorkbook();
+            CreationHelper createHelper = wb.getCreationHelper();
             XSSFSheet sheet = (XSSFSheet) wb.createSheet();
+            String tableName = rsMd.getTableName(1);
+            tableName = (tableName == null || "".equals(tableName)) ? "Output" : tableName;
+            wb.setSheetName(0, tableName);
+            CellStyle timeStyle = wb.createCellStyle();
+            timeStyle.setDataFormat(createHelper.createDataFormat().getFormat("d/m/yy h:mm"));
+            // Make the header line
             short rowNum = 0;
+            Row row = sheet.createRow(rowNum++);
+            for (int colNum = 1; colNum <= columnCount; colNum++) {
+                String columnName = rsMd.getColumnName(colNum);
+                Cell cell = row.createCell(colNum - 1);
+                cell.setCellValue(columnName);
+            }
+            // Data
             while (rs.next()) {
-                Row row = sheet.createRow(rowNum++);
+                row = sheet.createRow(rowNum++);
                 for (int colNum = 1; colNum <= columnCount; colNum++) {
-                    String value = rs.getString(colNum);
                     Cell cell = row.createCell(colNum - 1);
-                    cell.setCellValue(value);
+                    Object rawValue = rs.getObject(colNum);
+                    writeCell(cell, rawValue, timeStyle);
                 }
             }
             wb.write(console);
+        }
+
+        private void writeCell(Cell cell, Object rawValue, CellStyle timeStyle) throws Exception {
+            if (rawValue == null) {
+                cell.setCellValue((String) null);
+            } else if (rawValue instanceof Boolean) {
+                cell.setCellValue(((Boolean)rawValue).booleanValue());
+            } else if (rawValue instanceof Double) {
+                cell.setCellValue(((Double)rawValue).doubleValue());
+            } else if (rawValue instanceof Integer) {
+                cell.setCellValue(((Integer)rawValue).intValue());
+            } else if (rawValue instanceof Date) {
+                cell.setCellValue(((Date)rawValue));
+                cell.setCellStyle(timeStyle);
+            } else if (rawValue instanceof Clob) {
+                Clob tValue = (Clob) rawValue;
+                cell.setCellValue(tValue.getSubString(1, (int) tValue.length()));
+            } else {
+                cell.setCellValue(rawValue.toString());
+            }
         }
     },
     FLATXML {
